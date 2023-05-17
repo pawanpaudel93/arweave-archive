e1a609ae935bbb597e122af5f47b0a6cb480e351
Pawan Paudel <pawanpaudel93@gmail.com>
2023-05-17 17:45:11 +0545

    Update test

--- src/lib/archive.spec.ts
+++ src/lib/archive.spec.ts
@@ -1,15 +1,19 @@
 import ArLocal from 'arlocal';
+import Arweave from 'arweave';
 import test from 'ava';
-import dotenv from 'dotenv';
 
 import { Archive, ArchiveReturnType } from './archive';
 
-dotenv.config();
-
 test('Archive', async (t) => {
   const arLocal = new ArLocal(1984, false);
+  const arweave = Arweave.init({
+    host: 'localhost',
+    port: '1984',
+    protocol: 'http',
+  });
   await arLocal.start();
-  const archive = new Archive(process.env.JWK_PATH, 'http://localhost:1984', 'https://devnet.bundlr.network');
+  const jwk = await arweave.wallets.generate();
+  const archive = new Archive(jwk, 'http://localhost:1984', 'https://devnet.bundlr.network');
   const output: ArchiveReturnType = await archive.archiveUrl('https://github.com/pawanpaudel93');
   console.log(output);
   await arLocal.stop();
